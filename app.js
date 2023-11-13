93d61202ca4078f083c5444114cd8faeb85bd57c
Ganesh Sanjay Margale <108195526+GaneshMargale@users.noreply.github.com>
2023-11-13 18:40:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE'); // Allow all HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/v1/aptitude-dsa/user', userRouter);

428e25f83cdb73239b8249d2a026ff03f0c41003
Ganesh Sanjay Margale <108195526+GaneshMargale@users.noreply.github.com>
2023-11-20 22:35:04 +0530

    Add files via upload

--- Java/Main.java
+++ Java/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
-  public static void main(String[] args) {
-      System.out.println("q1");
-      Add q1 = new Add();
-      System.out.println(q1.add(1, 2));
-  }
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("C:\\Users\\Asus\\OneDrive\\Desktop\\Aptitude-DSA\\Java\\input.txt"));
+
+            while (scanner.hasNext()) {
+                // Read each line from the input file
+                String line = scanner.nextLine();
+                
+                // Split the line into individual inputs
+                String[] inputs = line.split(" ");
+                
+                // Convert inputs to integers or appropriate data types
+                int input1 = Integer.parseInt(inputs[0]);
+                int input2 = Integer.parseInt(inputs[1]);
+
+                // Use the inputs in your solution
+                Solution q = new Solution();
+                int result = q.addition(input1, input2);
+                System.out.println(result);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file not found.");
+        }
+    }
 }
+
---
+++ Java/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+  public int addition(int a, int b) {
+    return a + b;
+  }
+}

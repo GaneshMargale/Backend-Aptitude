d8228724e66e73e27d93c6e37b668bbd0c3c777e
Ganesh Sanjay Margale <108195526+GaneshMargale@users.noreply.github.com>
2023-11-22 21:02:47 +0530

    Add files via upload

--- Java/Main.java
+++ Java/Main.java
@@ -1,3 +1,5 @@
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -5,15 +7,25 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(new File("C:\\Users\\Asus\\OneDrive\\Desktop\\Aptitude-DSA\\Java\\input.txt"));
+            // Get the path to the current class file
+            Path currentClassPath = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toAbsolutePath();
+
+            // Construct the full path to the input.txt file
+            Path inputFilePath = currentClassPath.getParent().resolve("Java").resolve("input.txt");
+
+            // Create a File object from the path
+            File inputFile = inputFilePath.toFile();
+
+            // Use Scanner to read from the file
+            Scanner scanner = new Scanner(inputFile);
 
             while (scanner.hasNext()) {
                 // Read each line from the input file
                 String line = scanner.nextLine();
-                
+
                 // Split the line into individual inputs
                 String[] inputs = line.split(" ");
-                
+
                 // Convert inputs to integers or appropriate data types
                 int input1 = Integer.parseInt(inputs[0]);
                 int input2 = Integer.parseInt(inputs[1]);
@@ -27,7 +39,8 @@ public class Main {
             scanner.close();
         } catch (FileNotFoundException e) {
             System.err.println("Input file not found.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
-
